46903f17492d860eaa15238f9e98503d441bf25a
hendriks73 <hs@tagtraum.com>
2021-09-20 14:33:38 +0000

    Deploying to gh-pages from @ hendriks73/obstspot@0d557594cf0a6364eee6fcecd11948d25471790f 🚀

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"com.tagtraum.macos.spotify","l":"Application"},{"p":"com.tagtraum.macos.spotify","l":"EPlS"},{"p":"com.tagtraum.macos.spotify","l":"ImageData"},{"p":"com.tagtraum.macos.spotify","l":"Track"}]
